8b7d663fddf8e0de613cfd67d41c6f213f0c5350
wangzl <wangzl@fenqi.im>
2017-12-14 13:18:30 +0800

    update video url

--- templates/projects/video/video.js
+++ templates/projects/video/video.js
@@ -29,10 +29,12 @@ Video.prototype = {
 
 		var videoHtml = "";
 
+		var SWF_URL = "/templates/projects/video/CuPlayer.swf";
+
 		if(me.isFlash()){
 			videoHtml = '<video width="'+ settings.width +'px" height="'+ settings.height +'px"  src="'+ settings.url +'" poster="'+ settings.thumb +'" type="video/mp4" controls preload loop></video>';
 		}else{
-			videoHtml = '<embed type="application/x-shockwave-flash" src="/video/CuPlayer.swf" width="'+ settings.width +'" height="'+ settings.height +'" id="CuPlayer" name="CuPlayer" bgcolor="#000000" quality="high" allowfullscreen="true" allowscriptaccess="always" wmode="opaque" salign="lt" flashvars="CuPlayerFile='+ settings.url +'&amp;CuPlayerImage='+ settings.thumb +'&amp;CuPlayerShowImage=true&amp;CuPlayerWidth='+ settings.width +'&amp;CuPlayerHeight='+ settings.height +'&amp;CuPlayerAutoPlay=false&amp;CuPlayerAutoRepeat=false&amp;CuPlayerShowControl=true&amp;CuPlayerAutoHideControl=false&amp;CuPlayerAutoHideTime=6&amp;CuPlayerVolume=80&amp;CuPlayerGetNext=false"></embed>';
+			videoHtml = '<embed type="application/x-shockwave-flash" src="' + SWF_URL + '" width="'+ settings.width +'" height="'+ settings.height +'" id="CuPlayer" name="CuPlayer" bgcolor="#000000" quality="high" allowfullscreen="true" allowscriptaccess="always" wmode="opaque" salign="lt" flashvars="CuPlayerFile='+ settings.url +'&amp;CuPlayerImage='+ settings.thumb +'&amp;CuPlayerShowImage=true&amp;CuPlayerWidth='+ settings.width +'&amp;CuPlayerHeight='+ settings.height +'&amp;CuPlayerAutoPlay=false&amp;CuPlayerAutoRepeat=false&amp;CuPlayerShowControl=true&amp;CuPlayerAutoHideControl=false&amp;CuPlayerAutoHideTime=6&amp;CuPlayerVolume=80&amp;CuPlayerGetNext=false"></embed>';
 		};
 
 		$(settings.container).html(videoHtml);
